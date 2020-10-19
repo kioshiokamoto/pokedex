8334da88e468a5aa717ea68183e51d4ecd03cb24
Kioshi Okamoto <kjor29@gmail.com>
2020-10-18 21:41:27 -0500

    Build y modificacion de readme

---
+++ docs/precache-manifest.4c5cf2ad4472569df6f575a7459f2b31.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "478be92eacd342a5df609160e7dfb2e2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "77165af3cc6fd28879fb",
+    "url": "/static/css/main.99184d86.chunk.css"
+  },
+  {
+    "revision": "93e2d857f5caba8e0b49",
+    "url": "/static/js/2.98a55f20.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.98a55f20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77165af3cc6fd28879fb",
+    "url": "/static/js/main.deeba35e.chunk.js"
+  },
+  {
+    "revision": "250103716dfc89e5abb8",
+    "url": "/static/js/runtime-main.9ff9079d.js"
+  }
+]);
